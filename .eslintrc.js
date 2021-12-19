b029e2c137be12def110eb97f18f3c88725ed7e0
Yudi Supriyadi <yudisupriyadi123@gmail.com>
2021-12-19 10:27:44 +0700

    add Jest to ESlint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,20 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "commonjs": true,
-        "es2021": true
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 13,
+  },
+  rules: {},
+  overrides: [
+    {
+      files: ['**/*.test.js'],
+      env: {
+        jest: true,
+      },
     },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 13
-    },
-    "rules": {
-    }
+  ],
 };
